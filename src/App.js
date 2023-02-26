be084259b0c5413d9ff72933d656e18c9b514ba8
alfarrobeira <122460653+alfarrobeira@users.noreply.github.com>
2023-02-26 23:18:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState, useEffect } from "react";
+import Instructions from "./Instructions";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [on, setOn] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (on) {
+      setCount(count + 1);
+      setTimeout(() => {
+        setOn(false);
+      }, 2000);
+    }
+    console.log(on);
+  }, [on]);
+
+  useEffect(() => {
+    if (count >= 10) alert("No more light for you. Use a candle!");
+
+    console.log(`You clicked ${count} times`);
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="block">
+        <Instructions />
+      </div>
+
+      <div className={`block ${on ? "night" : ""}`}>
+        <a onClick={(e) => setOn(!on)}> turn me on! </a>
+        <div className="container">
+          <div className="bulb-light">
+            <div id="light" />
+
+            <div id="bulb">
+              <div className="bulb-top">
+                <div className="reflection" />
+              </div>
+              <div className="bulb-middle-1" />
+              <div className="bulb-middle-2" />
+              <div className="bulb-middle-3" />
+              <div className="bulb-bottom" />
+            </div>
+
+            <div id="base">
+              <div className="screw-top" />
+              <div className="screw-a" />
+              <div className="screw-b" />
+              <div className="screw-a" />
+              <div className="screw-b" />
+              <div className="screw-a" />
+              <div className="screw-b" />
+              <div className="screw-c" />
+              <div className="screw-d" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
